1cc3ff9687d25bac4433d8ffe499d30fa3ee72a8
Anurag <anurag.singh@5exceptions.com>
2025-07-25 12:47:11 +0530

    GSON to serialisation

--- android/src/main/java/com/truvideoreactturboimagesdk/TruvideoReactTurboImageSdkModule.kt
+++ android/src/main/java/com/truvideoreactturboimagesdk/TruvideoReactTurboImageSdkModule.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
-import com.google.gson.Gson
 import com.truvideo.sdk.image.TruvideoSdkImage
 import com.truvideo.sdk.image.interfaces.TruvideoSdkImageCallback
 import com.truvideo.sdk.image.model.TruvideoSdkImageCropInformation
